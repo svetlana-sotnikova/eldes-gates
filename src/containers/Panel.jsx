743dc0b961390cf546aee51a1150f176a1355dae
Svetlana Sotnikova <svetlana.data@gmail.com>
2021-07-23 14:40:09 +0300

    Получен deviceList из localStorage

--- src/containers/Panel.jsx
+++ src/containers/Panel.jsx
@@ -1,29 +1,21 @@
 import styled from '@emotion/styled';
-import { Button, Layout } from '../components';
+import { useEffect, useState } from 'react';
+import { Layout } from '../components';
 import { typography } from '../typography';
 
 export const Panel = () => {
-  const data = [
-    {
-      name: 'Поколение к1 шлагбаум, калитка',
-      buttonList: ['barrier1', 'gate1'],
-    },
-    {
-      name: 'Поколение к2 шлагбаум, калитка',
-      buttonList: ['barrier2', 'gate2'],
-    },
-  ];
+  const [deviceList, setDeviceList] = useState([]);
+
+  useEffect(() => {
+    setDeviceList(JSON.parse(localStorage.deviceList));
+  }, []);
 
   return (
     <Layout title="Eldes Gates">
-      {data.length > 0 ? (
-        data.map((item, index) => (
+      {deviceList.length > 0 ? (
+        deviceList.map((item, index) => (
           <Group key={index}>
             <h2 className={typography.h2}>{item.name}</h2>
-
-            {item.buttonList.map((button, index) => (
-              <Button key={index}>{button}</Button>
-            ))}
           </Group>
         ))
       ) : (
